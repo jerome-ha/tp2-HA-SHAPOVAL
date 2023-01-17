093bc0fcf8e464aa87040db36650f60a16b6c7fe
jerome-ha <jerome.ha@ensea.fr>
2023-01-17 09:11:44 +0100

    MARYNAAAA

--- Q2/serveur/go.sh
+++
@@ -1,7 +0,0 @@
-#env bash
-echo "Démarrage du serveur TFTP en écoute sur le port 1069"
-echo "Liste des fichiers disponibles:"
-ls -l o* z* alt* ensea.png
-echo "<Ctrl-c> pour arrêter..."
-
-./tftpd -c -L --address :1069 -s . -u $USER
--- brouillonavecfcts/fonctions.h
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netdb.h>
-
-
